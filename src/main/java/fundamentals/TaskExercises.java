bc017e1078a37607838df6bcac38ad4383294608
larisaigna <larissa_im@yahoo.com>
2021-01-30 13:38:25 +0200

    Task - new version of prime exercises

--- src/main/java/fundamentals/TaskExercises.java
+++ src/main/java/fundamentals/TaskExercises.java
@@ -311,6 +311,21 @@ public class TaskExercises {
         return isPrimeNo;
 
     }
+
+    private static boolean isPrime3(int nr) {
+        if (nr == 2) {
+            return true;
+        }
+        if (nr == 0 || nr == 1 || nr % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= nr; i += 2) {
+            if (nr % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 
